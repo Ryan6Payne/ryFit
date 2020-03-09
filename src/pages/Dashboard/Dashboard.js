bd18ebd5b26974a9569dd9cce70cf8c065c2852c
Ryan <ryan6payne@gmail.com>
2020-03-09 14:21:31 +0000

    Dashboard layout done

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,17 +1,56 @@
 import React from "react";
+import './Dashboard.scss';
+import { Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Dashboard() {
+export default function Dashboard() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>This page will include:</p>
-      <ul>
-        <li>All user information</li>
-        <li>Tips etc</li>
-        <li>Directed here if logged in</li>
-      </ul>
-    </div>
-  );
+    <div className="full-container-dashboard">
+      <div className="left-container-dashboard">
+        <Paper elevation={20} className={classes.paperL}>
+
+        </Paper>
+      </div>
+
+      <div className="right-container-dashboard">
+        <Paper elevation={0} className={classes.paperR}>
+          <Paper elevation={20} className={classes.paperRT}>
+
+          </Paper>
+          <Paper elevation={20} className={classes.paperRB}>
+
+          </Paper>
+        </Paper>
+      </div>
+    </div >
+
+  )
 }
 
-export default Dashboard;
+const useStyles = makeStyles({
+  paperL: {
+    width: '95%',
+    height: '95%',
+    border: '1px solid black'
+  },
+
+  paperR: {
+    width: '95%',
+    height: '95%',
+  },
+
+  paperRT: {
+    width: '100%',
+    height: '49%',
+    border: '1px solid black'
+  },
+  paperRB: {
+    width: '100%',
+    height: '47.75%',
+    marginTop: '2.5%',
+    border: '1px solid black'
+  }
+
+});
